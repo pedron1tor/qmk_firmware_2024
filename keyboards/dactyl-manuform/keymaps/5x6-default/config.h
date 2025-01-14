0609e3093e6b486c04c4af08c0d13345f2b14709
Pedro Nieto <picopedro8@gmail.com>
2025-01-13 22:30:29 -0600

    all the keys from the right half work!!!!

--- keyboards/dactyl-manuform/keymaps/5x6-default/config.h
+++ keyboards/dactyl-manuform/keymaps/5x6-default/config.h
@@ -19,7 +19,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 
 // #define MASTER_LEFT
-// #define MASTER_RIGHT
-#define EE_HANDS
+#define MASTER_RIGHT
 
 #include "../../config.h"
