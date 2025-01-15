0686556395e5b13245fb7155a6d48c45abf0aedf
Pedro Nieto <picopedro8@gmail.com>
2025-01-15 13:33:36 -0600

    update layout

--- keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
@@ -20,20 +20,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_ESC,                  KC_MINS ,KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_TAB,                  KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_LCTL,                  KC_ENT,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_CAPS,                  KC_ENT,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,                                  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-        KC_LALT, KC_GRV,  KC_LBRC, KC_SPC, KC_LSFT,                                 KC_RSFT, KC_SPC,   KC_QUOT, KC_EQL,  KC_LEFT,
-        LT(_FN, KC_DEL), KC_LGUI, KC_SPC,   KC_ENT,                                  KC_UP,   KC_RGUI, KC_PGUP, KC_PGDOWN
+        KC_LALT, KC_GRV,  KC_BSLS, KC_SPC, KC_LSFT,                               KC_RSFT, KC_SPC,   KC_QUOT, KC_EQL,  KC_LBRC,
+        LT(_FN, KC_FN0), KC_LGUI, KC_LALT,   KC_LCTRL,                                  KC_UP,   KC_DOWN, KC_RIGHT, KC_LEFT
     ),
-    
     [_FN] = KEYMAP_5x6(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-        _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP,      KC_DEL,  _______, KC_HOME, KC_UP,   KC_END,  KC_F12,
-        _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,      KC_ENT,  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,
-        RESET,   _______, _______, _______, _______,               KC_CALC, KC_MAIL, KC_MYCM, KC_WHOM, _______,
-        _______, _______, _______, _______, _______,               _______, _______, _______, _______, _______,
-        _______, _______, _______, _______,                        _______, _______, _______, _______
-    )
+            // left hand
+            KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,  KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,
+            _______, _______, KC_HOME, KC_UP,   KC_END,  KC_PGUP,      KC_DEL,  _______, KC_HOME, KC_UP,   KC_END,  KC_F12,
+            _______, _______, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,      KC_ENT,  _______, KC_LEFT, KC_DOWN, KC_RGHT, _______,
+            RESET,   _______, _______, _______, _______,                         KC_CALC, KC_MAIL, KC_MYCM, KC_WHOM, _______,
+            _______, _______, _______, _______, _______,                         _______, _______, _______, _______, KC_RBRC,
+            _______, _______, _______, _______,                                  _______, _______, _______, _______
+        ),
 }; // Added missing semicolon here
 
 void persistant_default_layer_set(uint16_t default_layer) {
