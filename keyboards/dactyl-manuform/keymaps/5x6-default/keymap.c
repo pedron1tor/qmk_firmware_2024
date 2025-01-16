153a4c3324f721b8fcf7110947d6dfa3c8ea83fb
Pedro Nieto <picopedro8@gmail.com>
2025-01-16 14:35:07 -0600

    keymap with mouse layer...

--- keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
+++ keyboards/dactyl-manuform/keymaps/5x6-default/keymap.c
@@ -10,6 +10,7 @@ extern keymap_config_t keymap_config;
 // entirely and just use numbers.
 #define _QWERTY 0
 #define _FN 1
+#define _MOUSE 2
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -19,12 +20,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = KEYMAP_5x6(
         // left hand
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_ESC,                  KC_MINS ,KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_TAB,                  KC_BSPC, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_CAPS,                  KC_ENT,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
+        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_TAB,                  KC_ENT, KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
+        KC_A,    KC_S,    KC_D,    KC_F,    KC_G, KC_CAPS,                  KC_BSPC,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
         KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,                                  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-        KC_LALT, KC_GRV,  KC_BSLS, KC_SPC, KC_LSFT,                               KC_RSFT, KC_SPC,   KC_QUOT, KC_EQL,  KC_LBRC,
-        LT(_FN, KC_FN0), KC_LGUI, KC_LALT,   KC_LCTRL,                                  KC_UP,   KC_DOWN, KC_RIGHT, KC_LEFT
+        LT(_FN, KC_FN0), KC_GRV,  KC_BSLS, KC_SPC, KC_LSFT,                               KC_RSFT, KC_SPC,   KC_QUOT, KC_EQL,  KC_LBRC,
+        MO(_MOUSE), KC_LGUI, KC_LALT,   KC_LCTRL,                                  KC_UP,   KC_DOWN, KC_RIGHT, KC_LEFT
+         // bottom left // top left // bottom right // top right 
     ),
+    
     [_FN] = KEYMAP_5x6(
             // left hand
             KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,         KC_F7,  KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,
@@ -34,9 +37,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             _______, _______, _______, _______, _______,                         _______, _______, _______, _______, KC_RBRC,
             _______, _______, _______, _______,                                  _______, _______, _______, _______
         ),
+        [_MOUSE] = KEYMAP_5x6(
+        // left hand
+        _______, _______, _______, _______, _______, KC_ACL0,                   _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, KC_WH_U, _______, KC_ACL2,                   _______, _______, _______, KC_MS_U, _______, _______, 
+        _______, _______, KC_BTN1, KC_BTN3, KC_BTN2, _______,                   _______, _______, KC_MS_L, KC_MS_D, KC_MS_R, _______, 
+        _______, _______, _______, KC_WH_D, _______,                                     _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______,                                    KC_ACL2, KC_WH_D, _______, _______, _______, 
+        _______, _______, _______, _______,                                              _______, _______, _______, _______
+    )
 }; // Added missing semicolon here
 
 void persistant_default_layer_set(uint16_t default_layer) {
     eeconfig_update_default_layer(default_layer);
     default_layer_set(default_layer);
-}
+} 
